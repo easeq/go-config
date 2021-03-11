c7033275ee64e2f1aaa9611f65112668f97926b2
ES <eon@earncef.com>
2021-03-11 10:05:27 +0530

    Initial commit

---
+++ config.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"errors"
+	"os"
+	"sync"
+
+	"github.com/Netflix/go-env"
+)
+
+var (
+	// ErrEnvConfigLoad returned when env config load results in an error
+	ErrEnvConfigLoad = errors.New("error loading env config")
+)
+
+// EnvConfig interface for managing env config
+type EnvConfig interface {
+	UnmarshalEnv(env.EnvSet) error
+}
+
+var (
+	// envOnce guards initialization by loadEnvironSet
+	envOnce sync.Once
+	// envSet holds the environment variables
+	envSet env.EnvSet
+)
+
+// NewEnvConfig adds env config to struct
+func NewEnvConfig(cfg EnvConfig) EnvConfig {
+	err := cfg.UnmarshalEnv(envSet)
+	if err != nil {
+		panic(ErrEnvConfigLoad)
+	}
+
+	return cfg
+}
+
+// EnvSet creates and returns a set of environment variables
+func EnvSet() env.EnvSet {
+	envOnce.Do(loadEnvironSet)
+	return envSet
+}
+
+// loadEnvironSet loads the env.EnvSet into envSet
+func loadEnvironSet() {
+	es, err := env.EnvironToEnvSet(os.Environ())
+	if err != nil {
+		panic(err)
+	}
+
+	envSet = es
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/easeq/go-config
+
+go 1.15
+
+require github.com/Netflix/go-env v0.0.0-20210215222557-e437a7e7f9fb
